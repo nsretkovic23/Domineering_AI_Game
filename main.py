eb9f01e3d7454bee240a8ae4158922c81bd731ef
nsretkovic <nsretkovic23@gmail.com>
2022-11-26 17:33:13 +0100

    Test Game initialization and print new state after some fields are modified

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 from ImportedScripts.CMDTextColorizer.ColorizeText import colored
 from Interface.StatePrinter import printCurrentState, clearConsole
 from Controllers.GameState import GameState
+from Interface.GameInitializer import intializeGame
 
 clearConsole()
 
+state:GameState = intializeGame(15,15)
+
 # Test matrix
 # 5 x 6 test matrix
-playingFields = [[" ", " ", " ", " ", " ", " "],
-                 [" ", " ", " ", " ", " ", " "],
-                 [" ", " ", "O", "O", " ", " "],
-                 [" ", "X", " ", " ", " ", " "],
-                 [" ", "X", " ", " ", " ", " "],]
+#playingFields = [[" ", " ", " ", " ", " ", " "],
+#                 [" ", " ", " ", " ", " ", " "],
+#                 [" ", " ", "O", "O", " ", " "],
+#                 [" ", "X", " ", " ", " ", " "],
+#                 [" ", "X", " ", " ", " ", " "],]
 
-state = GameState()
-state.rowDim = 5
-state.colDim = 6
-state.stateMatrix = playingFields
+
+printCurrentState(state)
+
+state.stateMatrix[5][5] = "X"
+state.stateMatrix[3][3] = "O"
 
 printCurrentState(state)
 
