84043e7e50154eeacd28d411a980eee9dea30b8d
nsretkovic <nsretkovic23@gmail.com>
2022-11-26 03:41:56 +0100

    Implement GameState class with initial variables

---
+++ Controllers/GameState.py
@@ -0,0 +1,8 @@
+class GameState:
+    def __init__(self) -> None:
+        self.playerSign = ""
+        self.cpuSign = ""
+        self.currentTurn = ""
+        self.rowDim = 0
+        self.colDim = 0
+        self.stateMatrix = []
