e6bc03915c457e652739f9c8260b6a2e4f974d93
nsretkovic <nsretkovic23@gmail.com>
2022-11-27 21:35:32 +0100

    Set game for classing playing and prepare test matrix for testing

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from Controllers.GameManager import isGameOver
+from Controllers.TurnController import playTurn
 from ImportedScripts.CMDTextColorizer.ColorizeText import colored
 from Interface.StatePrinter import printCurrentState, clearConsole
 from Controllers.GameState import GameState
@@ -5,22 +7,36 @@ from Interface.GameInitializer import intializeGame
 
 clearConsole()
 
+# Klasican tok igre
 state:GameState = intializeGame(15,15)
 
-# Test matrix
+isGameFinished = False
+
+printCurrentState(state)
+
+while not isGameFinished:
+    playTurn(state)
+    isGameFinished = isGameOver(state)[0]
+    printCurrentState(state)
+
+print(colored(f"GAME OVER! The winner is {isGameOver(state)[1]}", "green" ,attrs=["bold"]))
+
+
+# Testiranje stampanja proizvoljnog stanja igre
+
+#state:GameState = GameState
 # 5 x 6 test matrix
 #playingFields = [[" ", " ", " ", " ", " ", " "],
 #                 [" ", " ", " ", " ", " ", " "],
 #                 [" ", " ", "O", "O", " ", " "],
 #                 [" ", "X", " ", " ", " ", " "],
 #                 [" ", "X", " ", " ", " ", " "],]
-
-
-printCurrentState(state)
-
-state.stateMatrix[5][5] = "X"
-state.stateMatrix[3][3] = "O"
-
-printCurrentState(state)
+#
+#
+#state.stateMatrix = playingFields
+#state.rowDim = len(playingFields)
+#state.colDim = len(playingFields[0])
+#state.currentTurn = "X"
+#printCurrentState(state)
 
 
