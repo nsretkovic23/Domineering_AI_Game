46ad0159023e390fdcde13d29c9395cc073951b6
nsretkovic <nsretkovic23@gmail.com>
2022-12-12 01:27:07 +0100

    Getting all possible moves and all possible states
    - Get all possible moves returns a list with all valid places
    where domino can be placed based on who's currently playing
    - Get all possible next states
    returns a list with all new states that are created
    after playing every possible move  on top of the current state

--- Controllers/GameManager.py
+++ Controllers/GameManager.py
@@ -1,4 +1,9 @@
+from Controllers.ASCIIConverter import getASCIIFromNumberStartingAt0
 from Controllers.GameState import GameState
+from Controllers.TurnController import isHorizontalMoveValid, isVerticalMoveValid, playValidTurnInstantly
+import copy
+
+from Interface.StatePrinter import printCurrentState
 
 def canXPlay(state:GameState):
     for colIndex in range(0, state.colDim):
@@ -36,4 +41,25 @@ def isGameOver(state:GameState):
     else:
         return (False, "")
 
+def getAllPossibleMoves(state:GameState):
+    listOfPossibleMoves = []
 
+    for rowIndex in range(0,state.rowDim):
+        for colIndex in range(0, state.colDim):
+            if state.currentTurn == "X":
+                if isVerticalMoveValid(state.stateMatrix, state.rowDim, (rowIndex, colIndex)):
+                    listOfPossibleMoves.append([rowIndex, colIndex])
+            elif state.currentTurn == "O":
+                if isHorizontalMoveValid(state.stateMatrix, state.colDim, (rowIndex, colIndex)):
+                    listOfPossibleMoves.append([rowIndex, colIndex])
+
+    return listOfPossibleMoves
+
+def getAllPossibleNextStates(currentState:GameState):
+    possibleMoves = getAllPossibleMoves(state = currentState)
+    possibleNextStates = []
+
+    for i in range(0, len(possibleMoves)):
+        possibleNextStates.append(playValidTurnInstantly(currentState, possibleMoves[i][0], possibleMoves[i][1]))
+    
+    return possibleNextStates
