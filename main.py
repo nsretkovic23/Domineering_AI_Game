3615c632dcfb8cce5af9b07ceef050d8a0e7078b
nsretkovic <nsretkovic23@gmail.com>
2023-01-15 18:24:30 +0100

    Implement logic for playing player vs cpu

--- main.py
+++ main.py
@@ -1,48 +1,35 @@
+from Controllers.ASCIIConverter import getASCIIFromNumberStartingAt0
 from Controllers.GameManager import getAllPossibleMoves, getAllPossibleNextStates, isGameOver
-from Controllers.TurnController import playTurnWithInputs
+from Controllers.MinMax.MinMax import minimax
+from Controllers.TurnController import playTurnWithInputs, playValidTurnInstantly, undoLastTurn
 from ImportedScripts.CMDTextColorizer.ColorizeText import colored
 from Interface.StatePrinter import printAllPossibleMoves, printAllPossibleNextStates, printCurrentState, clearConsole
 from Controllers.GameState import GameState
-from Interface.GameInitializer import intializeGame
+from Interface.GameInitializer import getWhoPlaysFirst, intializeGame
 
 clearConsole()
 
-# Klasican tok igre
 state:GameState = intializeGame(15,15)
-
 isGameFinished = False
-
 printCurrentState(state)
 
 while not isGameFinished:
-    # Otkomentarisati za prikaz liste koji su naredni moguci potezi
-    #print(colored("Possible valid moves:","grey"))
-    #printAllPossibleMoves(getAllPossibleMoves(state))
-
-    # Otkomentarisati za graficki prikaz svih stanja koja su dobijena od svih narednih mogucih poteza
-    #print(colored("Possible next states:","grey"))
-    #printAllPossibleNextStates(getAllPossibleNextStates(state))
-
-    state = playTurnWithInputs(state)
     isGameFinished = isGameOver(state)[0]
+
+    if state.currentTurn == "X":
+        if state.playerSign == "X":
+            state = playTurnWithInputs(state)
+        else:
+            minMaxState = minimax(state, 1)
+            state = playValidTurnInstantly(state, minMaxState[0].minMaxGeneratedTurns[0][0], minMaxState[0].minMaxGeneratedTurns[0][1])
+    else:
+        if state.playerSign == "O":
+            state = playTurnWithInputs(state)
+        else:
+            minMaxState = minimax(state, 1)
+            state = playValidTurnInstantly(state, minMaxState[0].minMaxGeneratedTurns[0][0], minMaxState[0].minMaxGeneratedTurns[0][1])
     printCurrentState(state)
 
-print(colored(f"GAME OVER! The winner is {isGameOver(state)[1]}", "green" ,attrs=["bold"]))
 
+print(colored(f"GAME OVER! The winner is {isGameOver(state)[1]}", "green" if isGameOver(state)[1]=="X" else "red", attrs=["bold"]))
 
-# Testiranje stampanja proizvoljnog stanja igre
-
-#state:GameState = GameState
-# 5 x 6 test matrix
-#playingFields = [[" ", " ", " ", " ", " ", " "],
-#                 [" ", " ", " ", " ", " ", " "],
-#                 [" ", " ", "O", "O", " ", " "],
-#                 [" ", "X", " ", " ", " ", " "],
-#                 [" ", "X", " ", " ", " ", " "],]
-#
-#
-#state.stateMatrix = playingFields
-#state.rowDim = len(playingFields)
-#state.colDim = len(playingFields[0])
-#state.currentTurn = "X"
-#printCurrentState(state)
