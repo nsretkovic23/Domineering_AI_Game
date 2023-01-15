345dd73a214382e365c83ac50ee8e3a211032dad
nsretkovic <nsretkovic23@gmail.com>
2023-01-15 18:16:59 +0100

    Add additional attributes to GameState class

--- Controllers/GameState.py
+++ Controllers/GameState.py
@@ -6,3 +6,10 @@ class GameState:
         self.rowDim = 0
         self.colDim = 0
         self.stateMatrix = []
+        self.lastPlayedMove = []
+        # Keeps track of all played turns when state is being analyzed through minimax algorithm
+        # Almost always most important is the first element, because the state returned from minimax
+        # Contains best move, which is the first one, other ones(if any) are appended because of the dept analysis 
+        self.minMaxGeneratedTurns = []
+        self.lastPlayedX = []
+        self.lastPlayedO = []
